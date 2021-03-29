6586eb5d730a3017deb45bac95a03aecc594715b
Nataliia <nata.buchna@gmail.com>
2021-03-29 22:15:23 +0300

    task.BookStore

---
+++ Basics/BookStore.js
@@ -0,0 +1,30 @@
+const cart = [];
+const books = [
+    {id: 1, name: "Война и мир", price: 59.99},
+    {id: 2, name: "Гари Потный и загробный камнеь", price: 99.99},
+    {id: 3, name: "Лёлик и болик", price: 159.99},
+    {id: 4, name: "JS для чайинка", price: 0.99},
+];
+addToCart(1);
+addToCart(4);
+addToCart(3);
+checkout();
+//----------------------------------------------------------------
+
+ function addToCart(bookId) {
+    //Implement
+     // 1. find book by id
+     //    2. Add to cart
+
+}
+
+ function checkout(){
+    //TODO: print cart total price;
+    //TODO: print each element name and price
+}
+
+
+
+
+
+
--- Basics/loops.js
+++ Basics/loops.js
@@ -1,9 +1,25 @@
-const age = [10, 15, 43, 2];
+// const age = [10, 15, 43, 2];
+//
+// let sum = 0;
+//
+// for (let i = 0; i < age.length; i++) {
+//     sum = sum + age[i];
+// }
+//
+// console.log(sum);
+
+
+
+const cart = [{name:'Book1',price: 1},{name:'Book2',price: 5},{name:'Book3',price: 99},{name:'Book4',price: 0.99}];
 
 let sum = 0;
 
-for (let i = 0; i < age.length; i++) {
-    sum = sum + age[i];
+for (let i = 0; i < cart.length; i++) {
+
+    sum = sum + cart[i].price;
+
+    console.log("Вы добавили книгу", cart[i].name, "Цена:", cart[i].price, );
+
 }
 
-console.log(sum);
+console.log("Общая стоимость", sum);
